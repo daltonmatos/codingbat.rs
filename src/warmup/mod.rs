19879b2c3e29d42ea737ffce586ed05786a9e210
Dalton Barreto <daltonmatos@gmail.com>
2022-03-22 22:08:59 -0300

    warmup/monkey_trouble
    https://codingbat.com/prob/p120546

--- src/warmup/mod.rs
+++ src/warmup/mod.rs
@@ -1,3 +1,4 @@
 pub mod diff21;
+mod monkey_trouble;
 pub mod sleep_in;
 pub mod sum_double;
---
+++ src/warmup/monkey_trouble.rs
@@ -0,0 +1,40 @@
+/*
+
+https://codingbat.com/prob/p120546
+
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+
+monkey_trouble(True, True) → True
+monkey_trouble(False, False) → True
+monkey_trouble(True, False) → False
+*/
+
+fn monkey_trouble(a_smile: bool, b_smile: bool) -> bool {
+    (a_smile && b_smile) || (!a_smile && !b_smile)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_false_false() {
+        assert_eq!(true, monkey_trouble(false, false))
+    }
+
+    #[test]
+    fn test_false_true() {
+        assert_eq!(false, monkey_trouble(false, true))
+    }
+
+    #[test]
+    fn test_true_false() {
+        assert_eq!(false, monkey_trouble(true, false))
+    }
+
+    #[test]
+    fn test_true_true() {
+        assert_eq!(true, monkey_trouble(true, true))
+    }
+}
