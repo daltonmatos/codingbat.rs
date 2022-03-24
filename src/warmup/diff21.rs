4cfe013700c8d74e313e4be21b28c367a07efeff
Dalton Barreto <daltonmatos@gmail.com>
2022-03-23 22:59:44 -0300

    Movendo alguns testes para tests/*

--- src/warmup/diff21.rs
+++ src/warmup/diff21.rs
@@ -18,33 +18,3 @@ pub fn diff_21(target: i32) -> i32 {
     }
     _diff.abs()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn diff_under_21() {
-        assert_eq!(diff_21(10), 11)
-    }
-
-    #[test]
-    fn diff_exact_21() {
-        assert_eq!(diff_21(21), 0)
-    }
-
-    #[test]
-    fn diff_obove_21() {
-        assert_eq!(diff_21(30), 18)
-    }
-
-    #[test]
-    fn diff_negative() {
-        assert_eq!(diff_21(-1), 22)
-    }
-
-    #[test]
-    fn diff_much_above() {
-        assert_eq!(diff_21(50), 58)
-    }
-}
